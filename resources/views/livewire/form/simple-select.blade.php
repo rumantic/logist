ee4cf43942f15bd865fb2e515072fe468f8f456c
Kondin Dmitriy <dkondin@gmail.com>
2023-09-18 21:45:13 +0700

    убираем экспериментальные элементы

--- resources/views/livewire/form/simple-select.blade.php
+++
@@ -1,6 +0,0 @@
-<select wire:model.defer="{{$model_name}}" class="form-control @error($model_name) is-invalid @enderror" name="{{$model_name}}" placeholder="{{ $title }}">
-    <option value="0">не выбрано</option>
-    @foreach( $options as $item )
-        <option value="{{$item['value']}}" wire:key="opt-{{ $item['value'] }}">{{$item['description']}}</option>
-    @endforeach
-</select>
